a2d12780df6784a2cad66ffd5192a243150d1e75
Xasbus <davidbilliot0828.gmail.com>
2024-10-18 12:04:18 -0500

    Fixed Typo Error

--- forms.js
+++ forms.js
@@ -1,9 +1,10 @@
 function myProcessForm(event) {
+    debugger;
     event.preventDefault(); //Prevents page from refreshing after submitting a form.
     const form = event.target;
     const myPasswordInput = form[0];
     const firstNameInput = form[1];
     const lastNameInput = form[2];
-    const value = firstNameInput.value;
+    const value = myPasswordInput.value;
     output(` The password is ${value}<br>`);
 }
